4452a63b6a1de0e828566b3fe628b2585cf8cfa6
TAKASHI ARAKI <takashi@MacBook-Pro.local>
2020-08-21 23:26:52 +0900

    create operation logs

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,6 +1,8 @@
 import React, { useContext, useState } from 'react';
-import { CREATE_EVENT, DELETE_ALL_EVENTS } from '../actions';
+import { CREATE_EVENT, DELETE_ALL_EVENTS, ADD_OPERATION_LOG, DELETE_ALL_OPERATION_LOGS } from '../actions';
 import AppContext from '../contexts/AppContext';
+import { timeCurrentIso8601 } from '../utils';
+
 
 const EventForm = () => {
   const { dispatch, state } = useContext(AppContext);
@@ -16,6 +18,12 @@ const EventForm = () => {
         body
       });
 
+      dispatch({
+        type: ADD_OPERATION_LOG,
+        description: 'イベントを作成しました',
+        operatedAt: timeCurrentIso8601(),
+      })
+
       setTitle('');
       setBody('');
   };
@@ -26,9 +34,15 @@ const EventForm = () => {
     const result = window.confirm('本当に全てのイベントを削除しますか？');
     
     if(result)
-    dispatch({
-      type: DELETE_ALL_EVENTS
-    })
+      dispatch({
+        type: DELETE_ALL_EVENTS
+      });
+      dispatch({
+        type: ADD_OPERATION_LOG,
+        description: '全てのイベントを削除しました。',
+        operatedAt: timeCurrentIso8601(),
+      })
+
   };
 
   const uncreatable = title === '' || body === '';
